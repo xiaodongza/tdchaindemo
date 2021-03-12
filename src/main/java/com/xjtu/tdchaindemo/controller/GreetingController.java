7447283fd03acf786aba9973863125d732d1a5e9
hc <79045803@qq.com>
2021-03-12 15:29:34 +0800

    modify pom

--- src/main/java/com/xjtu/tdchaindemo/controller/GreetingController.java
+++ src/main/java/com/xjtu/tdchaindemo/controller/GreetingController.java
@@ -1,19 +1,21 @@
 package com.xjtu.tdchaindemo.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import com.xjtu.tdchaindemo.service.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
+@CrossOrigin(origins = {"*"}, allowCredentials = "true")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @PostMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
